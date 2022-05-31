f1e6c893479ad1d9a72b868b2783fdcf8fdcfe72
Gabriel Elias <gabriel_elias9@hotmail.com>
2022-05-31 08:37:18 -0300

    Modify api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const routes = require('./routes/routes')
+const routes = require('./src/routes/routes')
 const mongoose = require('mongoose')
 
 app.use(cors())
@@ -10,15 +10,16 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(routes)
 
+const port = process.env.PORT || 3000
 
-const url = 'mongodb+srv://gabriel:LzxLySm9nKHQQeSI@apicluster.os8bk.mongodb.net/usersDatabase?retryWrites=true&w=majority'
+const mongo = 'mongodb+srv://gabriel:LzxLySm9nKHQQeSI@apicluster.os8bk.mongodb.net/usersDatabase?retryWrites=true&w=majority'
 
 mongoose
-.connect(url)
+.connect(mongo)
 .then(() =>{ 
     console.log('Conenctou ao Mongo')
-    app.listen(3030, ()=> { 
-        console.log('Running')
+    app.listen(port, ()=> { 
+        console.log('Running in', port)
     })
 })
 .catch((err) => {
